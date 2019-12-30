8c75e044e43c066f76008876149d27bada327452
SkittlePox <bspiegel11@gmail.com>
2019-12-29 20:03:40 -0500

    zeep

--- Grammar.py
+++ Grammar.py
@@ -10,17 +10,6 @@ class CombinatoryRule():
         self.test = test
         self.operate = operate
 
-    # def test(self, syntactic_category_a, syntactic_category_b):
-    #
-    #     return self.testfunc(syntactic_category_a, syntactic_category_b)
-    #
-    # def operate(self, lexical_entry_a, lexical_entry_b):
-    #     """
-    #     This function takes whole lexical entries
-    #     Returns the resulting lexical entry after applying this rule
-    #     """
-    #     return self.operation(lexical_entry_a, lexical_entry_b)
-
 class Grammar():
     def __init__(self, rules):
         self.rules = rules
@@ -28,8 +17,12 @@ class Grammar():
 class DCGrammar(Grammar):
     def __init__(self):
         rules = []
-        # R1a_test = lambda a, b: b.rhs == a.lhs
-        # print(R1a_test())
-        # rules.append(CombinatoryRule(test=lambda a,b: a == b)
+        R1a_test = lambda a, b: b.rhs == a.lhs
+        def R1a_operate(a, b):
+            newName = f"{a.english} {b.english}"
+            newCat = b.category.lhs
+            newSem = b.type.rhs
+            return LexicalEntry(newName, newCat, newSem)
+        rules.append(CombinatoryRule(test=R1a_test, operate=R1a_operate)
 
         Grammar.__init__(self, rules)
