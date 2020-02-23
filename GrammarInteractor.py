1d462bd7eaf2228481515fc1832cb8a861b5cd33
SkittlePox <bspiegel11@gmail.com>
2020-02-23 17:04:48 -0500

    Updated GrammarInteractor

--- GrammarInteractor.py
+++ GrammarInteractor.py
@@ -1,19 +1,35 @@
 from itertools import compress
 import copy
+from Grammar import *
 
 class GrammarInteractor():
     def __init__(self, grammar):
         self.grammar = grammar
 
+    def possible_unaries(self, a):
+        un_rules = list(filter(lambda x: isinstance(x, UnaryRule), self.grammar.rules.values()))
+        rls = []
+        for r in un_rules:
+            if r.test(a):
+                rls.append(r)
+        return rls
+
     def possible_combinations(self, a, b):
-        selectors = list(map(lambda x: x.test(a, b), self.grammar.rules.values()))
-        # data = map(lambda x: str(x), self.grammar.rules)
-        return list(compress(self.grammar.rules.values(), selectors))
+        co_rules = list(filter(lambda x: isinstance(x, CombinatoryRule), self.grammar.rules.values()))
+        rls = []
+        for r in co_rules:
+            if r.test(a, b):
+                rls.append(r)
+        return rls
 
     def populate_lexicon(self, lexicon, layers=4):
         for i in range(layers):
             entries = copy.copy(lexicon.entries)
             for a in entries:
+                unaryRules = self.possible_unaries(a)
+                if len(unaryRules) is not 0:
+                    un_entries = list(map(lambda x: x(a), unaryRules))
+                    lexicon.add(un_entries)
                 for b in entries:
                     combinatoryRules = self.possible_combinations(a, b)
                     if len(combinatoryRules) is not 0:
